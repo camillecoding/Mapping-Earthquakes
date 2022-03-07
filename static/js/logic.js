c758bdcb1f16e593229278a76cec6cbb954065ec
Camille May <95657458+camillecoding@users.noreply.github.com>
2022-03-07 09:09:15 -0500

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,28 +1,29 @@
 //add console.log to confirm whether code works
 console.log("working");
 
-//create the map object with a cener and zoom level
-let map =L.map('mapid').setView([40.7, -94.5], 4);
+//add marker on the map for somewhere between LAX and SFO
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+//coordinates for each point in the line
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+let polyline = L.polyline(line, {color: 'yellow'}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets/dark-v10',
+    id: 'mapbox/satellite-streets-v11',
     maxZoom: 18,
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-//add marker on the map for Los Angeles, CA
-let cityData = cities;
 
-//loop through the cities array and drop a marker on each city
-cities.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
+
+
