5c26cefafcce3ebfe7f11340f327affcc85ca306
Camille May <95657458+camillecoding@users.noreply.github.com>
2022-03-06 20:53:25 -0500

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -15,8 +15,39 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 streets.addTo(map);
 
 //add marker on the map for Los Angeles, CA
-L.circleMarker([34.0522, -118.2437], {
-    color: 'black',
-    fillColor: '#ffffa1',
-    radius: 300
-}).addTo(map);
+let cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY", 
+    population: 8398748
+},
+{
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+},
+{
+    location: [29.7604, -95.3698],
+    city: "Houston", 
+    state: "TX",
+    population: 2325502
+},
+{
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+},
+{
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+}
+];
+
+//loop through the cities array and drop a marker on each city
+cities.forEach(function(city) {
+    console.log(city)
+});
