e216d60f6fee26dbe6a08dd746a6e508272a484d
Camille May <95657458+camillecoding@users.noreply.github.com>
2022-03-07 08:39:02 -0500

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 //create the map object with a cener and zoom level
-let map =L.map('mapid').setView([40.7, -94.5], 14);
+let map =L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -15,39 +15,14 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 streets.addTo(map);
 
 //add marker on the map for Los Angeles, CA
-let cities = [{
-    location: [40.7128, -74.0059],
-    city: "New York City",
-    state: "NY", 
-    population: 8398748
-},
-{
-    location: [41.8781, -87.6298],
-    city: "Chicago",
-    state: "IL",
-    population: 2705994
-},
-{
-    location: [29.7604, -95.3698],
-    city: "Houston", 
-    state: "TX",
-    population: 2325502
-},
-{
-    location: [34.0522, -118.2437],
-    city: "Los Angeles",
-    state: "CA",
-    population: 3990456
-},
-{
-    location: [33.4484, -112.0740],
-    city: "Phoenix",
-    state: "AZ",
-    population: 1660272
-}
-];
+let cityData = cities;
 
 //loop through the cities array and drop a marker on each city
 cities.forEach(function(city) {
     console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
 });
